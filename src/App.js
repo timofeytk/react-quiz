4dff094dcd6f19c7c2179dd5b35e2cbaee639657
Тимур Токтошев <timurtoktosev@MacBook-Air-Timur.local>
2023-04-26 16:30:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const questions = [
+  {
+    question: 'Назовите столицу США?',
+    answers: ['Вашингтон', 'Москва', 'Дубаи'],
+    correct: 0
+  },
+  {
+    question: 'Третья планета от Солнца?',
+    answers: ['Марс', 'Венера', 'Земля'],
+    correct: 2
+  },
+  {
+    question: 'Национальная валюта Китая?',
+    answers: ['Йена', 'Юань', 'Рубль'],
+    correct: 1
+  },
+  {
+    question: 'В 1 квадратном километре сколько квадратных метров?',
+    answers: ['Тысяча', 'Сто тысяч', 'Миллион'],
+    correct: 2
+  },
+  {
+    question: 'Какой из вариантов это город-государство?',
+    answers: ['Ватикан', 'Албания', 'Ирландия'],
+    correct: 0
+  }
+];
+
+function Result(){
+  return(
+    <div className="result">
+      <p>Вы отгадали 3 ответа из 5</p>
+      <button>Начать заново</button>
+    </div>
+  );
+}
+
+function Quiz({data}){
+  return(
+    <div className="quizBlock">
+      <div className="progress"></div>
+      <div className="quizBlock__inner">
+        <h2>{data.question}</h2>
+        <ul>
+          {data.answers.map((text, index) => (
+            <li key={index}>{text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const question = questions[step];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Quiz data={question}/>
     </div>
   );
 }
